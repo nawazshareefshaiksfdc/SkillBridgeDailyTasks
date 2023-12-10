a107adaa90380bc8a757d7cb9bd978ad37606db8
nawazshareefshaiksfdc <147690940+nawazshareefshaiksfdc@users.noreply.github.com>
2023-12-11 01:51:25 +0530

    updated

--- ForLoopFolder/problem3.js
+++ ForLoopFolder/problem3.js
@@ -1,22 +1,23 @@
 function sortInventory(inventory) {
-  let swapped;
-  do {
-    swapped = false;
-    for (let i = 0; i < inventory.length - 1; i++) {
+  let isSwapped;
+  for (let Pass = 0; Pass < inventory.length - 1; Pass++) {
+    isSwapped = false;
+    for (let i = 0; i < inventory.length - 1 - Pass; i++) {
       if (inventory[i].car_model > inventory[i + 1].car_model) {
         const temp = inventory[i];
         inventory[i] = inventory[i + 1];
         inventory[i + 1] = temp;
-        swapped = true;
+        isSwapped = true;
       }
     }
-  } while (swapped);
+    if (!isSwapped) {
+      break;
+    }
+  }
 
   const sortedModels = [];
-  let index = 0;
-  while (index < inventory.length) {
-    sortedModels[index] = inventory[index].car_model;
-    index++;
+  for (let i = 0; i < inventory.length; i++) {
+    sortedModels[i] = inventory[i].car_model;
   }
 
   return sortedModels;
