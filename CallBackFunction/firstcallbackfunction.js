82a2b0372e23a78a57e7b031f900d35c97e5bffb
nawazshareefshaiksfdc <147690940+nawazshareefshaiksfdc@users.noreply.github.com>
2023-12-11 09:01:53 +0530

    callbackfunctions Intiation

---
+++ CallBackFunction/firstcallbackfunction.js
@@ -0,0 +1,7 @@
+const testObject = { name: 'Bruce Wayne', age: 36, location: 'Gotham' };
+
+const keysArray = Object.keys(testObject);
+console.log(keysArray);
+
+const valuesArray = Object.values(testObject);
+console.log(valuesArray);
